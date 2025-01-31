9b898fe69ea5bed0a66bd0aab753cbb2414bce75
sorcererm83 <alex.boy.gonzalez@gmail.com>
2025-01-31 15:20:56 +0000

    proyecto listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const suits = ["♦", "♥", "♠", "♣"];
+
+function randomElement(arr) {
+  const randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
+}
+
+function randomCardGenerator() {
+  const number = randomElement(numbers);
+  const suit = randomElement(suits);
+
+  if (suit === "♦" || suit === "♥") {
+    document.getElementById("card").style.color = "red";
+  } else if (suit === "♠" || suit === "♣") {
+    document.getElementById("card").style.color = "black";
+  }
+
+  document.getElementById("upper").textContent = suit;
+  document.getElementById("central").textContent = number;
+  document.getElementById("low").textContent = suit;
+}
+
+randomCardGenerator();
+document
+  .getElementById("Button")
+  .addEventListener("click", randomCardGenerator);
+setInterval(randomCardGenerator, 10000);
